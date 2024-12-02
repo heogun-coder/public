b00ae5011eafbb817741c5db2da9276eaf93f435
unknown <heochangick@gmail.com>
2024-12-02 09:35:14 +0900

    v2

---
+++ matrix_crypto/key_generate.py
@@ -0,0 +1,40 @@
+def generate_key(matrix_size, matrix):
+    L = matrixify(matrix_size)
+    for x in range(matrix_size):
+        L[x][x] = 1
+
+    U = matrixify(matrix_size)
+    for x in range(matrix_size):
+        for y in range(matrix_size):
+            U[x][y] = matrix[x][y]
+
+    for x in range(matrix_size):
+        for y in range(0, x):
+            if U[x][y] != 0:
+                div = U[x][y] / U[y][y]
+                L[x][y] = div
+                for z in range(y, matrix_size):
+                    U[x][z] -= U[y][z] * div
+
+    return L, U
+
+
+def matrixify(matrix_size):
+    matrix = [0 for _ in range(matrix_size)]
+    for i in range(matrix_size):
+        matrix[i] = [0 for _ in range(matrix_size)]
+    return matrix
+
+
+def set_matrix(sentence):
+    matrix_size = len(sentence)
+
+    matrix = matrixify(matrix_size)
+
+    pass
+
+    return matrix
+
+
+sentence = "helloworld"
+key_public, key_private = generate_key(10, set_matrix(sentence))
