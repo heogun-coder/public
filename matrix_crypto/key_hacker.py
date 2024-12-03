cac7554e5105d1b095da9c39f6fcc81d08cc862e
unknown <heochangick@gmail.com>
2024-12-03 13:17:54 +0900

    v2

--- matrix_crypto/key_hacker.py
+++ matrix_crypto/key_hacker.py
@@ -2,6 +2,11 @@ def brute_force_key(matrix_size, matrix):
     pass
 
 
-def solve_key(matrix, n, det):
+def solve_key(matrix, n, det, prime_factors):  # 소인수분해를 통해 약수 판별
+
+    pass
+
+
+def get_prime_factors(list):
 
     pass
